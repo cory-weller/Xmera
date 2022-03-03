3d81beeea7f95bcc6fa4bbb21d69571e83c48d27
cory-weller <cory.a.weller@gmail.com>
2022-03-03 11:39:40 -0500

    update notes in formatCodons.py

--- src/formatCodons.py
+++ src/formatCodons.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
-# reformats the codon table codonTable.txt, from
-# the Codon Usage Database: https://www.kazusa.or.jp/codon/cgi-bin/showcodon.cgi?species=4932&aa=1&style=N
+# reformats codon table, e.g. from https://www.kazusa.or.jp/codon/cgi-bin/showcodon.cgi?species=4932&aa=1&style=N
 
 with open("codonTable.txt", "r") as infile:
     inputText = infile.read().replace("(","").replace(")","").split()
