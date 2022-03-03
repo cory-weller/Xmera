68b8e964500b688a4c106f9fc755234124d1b8d5
cory-weller <cory.a.weller@gmail.com>
2022-03-03 14:02:03 -0500

    add infile arg

--- src/formatCodons.py
+++ src/formatCodons.py
@@ -2,10 +2,15 @@
 
 # reformats codon table, e.g. from https://www.kazusa.or.jp/codon/cgi-bin/showcodon.cgi?species=4932&aa=1&style=N
 
-with open("codonTable.txt", "r") as infile:
+import sys
+
+codonInput = sys.argv[1]
+
+with open(codonInput, "r") as infile:
     inputText = infile.read().replace("(","").replace(")","").split()
 
 entries = [inputText[x:(x+5)] for x in range(0, len(inputText), 5)]
 
+print('\t'.join(['triplet', 'amino_acid', 'fraction', 'frequency_per_thousand', 'number']))
 for row in entries:
     print('\t'.join([str(x) for x in row]))
